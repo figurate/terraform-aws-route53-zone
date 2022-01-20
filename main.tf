d58216397e8f704a9428207681d7e4633c13691e
Ben Fortuna <benfortuna@users.noreply.github.com>
2022-01-20 15:15:50 +1100

    Added zone configuration

--- main.tf
+++ main.tf
@@ -5,3 +5,42 @@
  *
  * ![main workflow](https://github.com/figurate/terraform-aws-route53-zone/actions/workflows/main.yml/badge.svg)
  */
+data "aws_vpc" "vpc" {
+  count = var.vpc_name != null ? 1 : 0
+  filter {
+    name   = "tag:Name"
+    values = [var.vpc_name]
+  }
+}
+
+resource "aws_route53_zone" "zone" {
+  name = var.name
+  dynamic "vpc" {
+    for_each = var.vpc_name != null ? [1] : []
+    content {
+      vpc_id = data.aws_vpc.vpc[0].id
+    }
+  }
+}
+
+resource "aws_route53_record" "record" {
+  count   = length(var.records)
+  name    = var.records[count.index][0]
+  type    = var.records[count.index][1]
+  records = var.records[count.index][2]
+  ttl     = 300
+  zone_id = aws_route53_zone.zone.id
+}
+
+resource "aws_route53_record" "alias" {
+  #checkov:skip=CKV2_AWS_23:A record attached resource specified at root level
+  count = length(var.aliases)
+  name  = var.aliases[count.index][0]
+  type  = "A"
+  alias {
+    evaluate_target_health = false
+    name                   = var.aliases[count.index][1]
+    zone_id                = var.aliases[count.index][2]
+  }
+  zone_id = aws_route53_zone.zone.id
+}
